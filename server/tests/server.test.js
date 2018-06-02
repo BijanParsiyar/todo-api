b58ad1e834e9fbf9faec360fe9366b8492c54dbb
BijanParsiyar <bparsiyar@gmail.com>
2018-06-02 16:04:20 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -296,11 +296,33 @@ describe("POST /users/login", () => {
         expect(res.headers["x-auth"]).toBeFalsy();
       })
       .end((err, res) => {
+        // .END is for asyncrhonous assertions ( the one below / the mongoose query )
         if (err) {
           return done(err);
         }
 
         User.findById(users[1]._id)
+          .then(user => {
+            // expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
+
+describe("DELETE /users/me/token", () => {
+  it("should remove auth token on logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
           .then(user => {
             // expect(user.tokens.length).toBe(0);
             done();
